a9df44ba1e543c74a8faf7f84fa22e2ed88fff24
Ryan Cooper <ryangcooper96@gmail.com>
2022-09-01 20:48:06 +0100

    Added Routes to App.js and imported all pages and Header, Footer components.

--- met-musesum-of-art/src/App.js
+++ met-musesum-of-art/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import DetailedView from "./pages/DetailedView";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Library from "./Library";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="" element={<About />} />
+        <Route path="" element={<Library />} />
+        <Route path="" element={<Favorites />} />
+        <Route path="" element={<DetailedView />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

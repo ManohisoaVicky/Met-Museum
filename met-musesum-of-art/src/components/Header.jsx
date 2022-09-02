a2a019e27ccfb0c494e73a44d99a48c644efa91e
Manohisoa <manohisoarajaonarivony@gmail.com>
2022-09-03 00:53:51 +0300

    Updated the header jsx and css, added hover effect on nav links

--- met-musesum-of-art/src/components/Header.jsx
+++ met-musesum-of-art/src/components/Header.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 import "../styles/Header.css"
 
 function Header() {
   return (
     <header className='Header'>
-        <div className='logo'>Logo</div>
+        <Link to="/" className='logo-link'>
+            <div className='logo'>Logo</div>
+        </Link>
         <nav>
-            <Link to="/">
-                <span>Home</span>
-            </Link>
-            <Link to="/about">
-                <span>About</span>            
-            </Link>
-            <Link to="/library">
-                <span>Library</span>            
-            </Link>
-            <Link to="/favorites">
-                <span>Favorites</span>            
-            </Link>
+            <ul>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/library">Library</CustomLink>
+                <CustomLink to="/favorites">Favorites</CustomLink>
+            </ul>
         </nav>
     </header>
   )
+}
+
+function CustomLink({to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const active = useMatch({ path: resolvedPath.pathname, end: true})
+
+    return (
+        <li className={active? "active": ""}>
+            <Link to={to} {... props} className="nav-link">
+                {children}
+            </Link>
+        </li>
+    )
 }
 
 export default Header
